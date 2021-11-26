78c716411491eccecbdfc22f1687cd970ae7474e
ShelbyChai <chaixiangzhi8262@gmail.com>
2021-11-26 19:01:35 +0800

    'Add build file and module-info.java'

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module COMP2042.CW.hfyxc3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens brickdestroyer to javafx.fxml;
+    exports brickdestroyer;
+    exports brickdestroyer.GameBoard;
+    opens brickdestroyer.GameBoard to javafx.fxml;
+    exports brickdestroyer.HomeMenu;
+    opens brickdestroyer.HomeMenu to javafx.fxml;
+}
